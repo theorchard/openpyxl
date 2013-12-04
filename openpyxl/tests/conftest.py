0b8b4a843f3929a70c56d773576d922f0f03cd53
TWAC <TWAC@localhost>
2013-12-04 17:53:43 +0100

    support oneCellAnchor for images split out anchor writer

---
+++ openpyxl/tests/conftest.py
@@ -0,0 +1,105 @@
+# Fixtures (pre-configured objects) for tests
+
+import pytest
+
+# objects under test
+
+@pytest.fixture
+def NumberFormat():
+    from openpyxl.styles import NumberFormat
+    return NumberFormat
+
+
+@pytest.fixture
+def Workbook():
+    from openpyxl import Workbook
+    return Workbook
+
+# Charts
+
+@pytest.fixture
+def Chart():
+    from openpyxl.chart import Chart
+    return Chart
+
+
+@pytest.fixture
+def GraphChart():
+    from openpyxl.chart import GraphChart
+    return GraphChart
+
+
+@pytest.fixture
+def Axis():
+    from openpyxl.chart import Axis
+    return Axis
+
+
+@pytest.fixture
+def PieChart():
+    from openpyxl.chart import PieChart
+    return PieChart
+
+
+@pytest.fixture
+def LineChart():
+    from openpyxl.chart import LineChart
+    return LineChart
+
+
+@pytest.fixture
+def BarChart():
+    from openpyxl.chart import BarChart
+    return BarChart
+
+
+@pytest.fixture
+def ScatterChart():
+    from openpyxl.chart import ScatterChart
+    return ScatterChart
+
+
+@pytest.fixture
+def Reference():
+    from openpyxl.chart import Reference
+    return Reference
+
+
+@pytest.fixture
+def Serie():
+    from openpyxl.chart import Serie
+    return Serie
+
+
+@pytest.fixture
+def ErrorBar():
+    from openpyxl.chart import ErrorBar
+    return ErrorBar
+
+# utility fixtures
+
+@pytest.fixture
+def ws(Workbook):
+    wb = Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = 'data'
+    return ws
+
+
+@pytest.fixture
+def ten_row_sheet(ws):
+    for i in range(10):
+        ws.cell(row=i, column=0).value = i
+    return ws
+
+
+@pytest.fixture
+def ten_column_sheet(ws):
+    ws.append(list(range(10)))
+    return ws
+
+from openpyxl.shared.xmltools import Element
+
+@pytest.fixture
+def root_xml():
+    return Element("test")
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""Setup script for packaging openpyxl.
+
+Requires setuptools.
+
+To build the setuptools egg use
+    python setup.py bdist_egg
+and either upload it to the PyPI with:
+    python setup.py upload
+or upload to your own server and register the release with PyPI:
+    python setup.py register
+
+A source distribution (.zip) can be built with
+    python setup.py sdist --format=zip
+
+That uses the manifest.in file for data files rather than searching for
+them here.
+
+"""
+
+import sys
+import warnings
+
+if sys.version_info < (2, 6):
+    raise Exception("Python >= 2.6 is required.")
+
+from setuptools import setup, Extension, find_packages
+import openpyxl  # to fetch __version__ etc
+
+
+setup(name = 'openpyxl',
+    packages = find_packages(),
+    # metadata
+    version = openpyxl.__version__,
+    description = "A Python library to read/write Excel 2007 xlsx/xlsm files",
+    long_description = 'openpyxl is a pure python reader and writer of '
+        'Excel OpenXML files.  It is ported from the PHPExcel project',
+    author = openpyxl.__author__,
+    author_email = openpyxl.__author_email__,
+    url = openpyxl.__url__,
+    license = openpyxl.__license__,
+    download_url = openpyxl.__downloadUrl__,
+    classifiers = ['Development Status :: 4 - Beta',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3'],
+    )
