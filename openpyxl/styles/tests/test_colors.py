937a2876872ac5a22d9b1b2670a394d538f5248b
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-04-06 16:38:01 +0200

    Initial test for colors.

---
+++ openpyxl/styles/tests/test_colors.py
@@ -0,0 +1,13 @@
+from openpyxl.styles.colors import Color
+import pytest
+
+class TestColor:
+
+    def test_ctor(self):
+        c = Color()
+        assert c.index == "FF000000"
+
+    def test_validation(self):
+        c = Color()
+        with pytest.raises(TypeError):
+            c.index = 4
