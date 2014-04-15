65ba35319569b7b503b38ca5ef261bf361ff89ac
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-04-15 22:08:10 +0200

    Moved tests to chart module

--- openpyxl/tests/test_chart_writer.py
+++ openpyxl/tests/test_chart_writer.py
@@ -18,47 +18,6 @@ from .schema import chart_schema
 
 
 
-@pytest.fixture
-def pie_chart(ws, Reference, Series, PieChart):
-    ws.title = 'Pie'
-    for i in range(1, 5):
-        ws.append([i])
-    chart = PieChart()
-    values = Reference(ws, (0, 0), (9, 0))
-    series = Series(values, labels=values)
-    chart.add_serie(series)
-    return chart
-
-
-class TestPieChartWriter(object):
-
-    def test_write_chart(self, pie_chart):
-        """check if some characteristic tags of PieChart are there"""
-        cw = PieChartWriter(pie_chart)
-        cw._write_chart()
-
-        tagnames = ['{%s}pieChart' % CHART_NS,
-                    '{%s}varyColors' % CHART_NS
-                    ]
-        root = safe_iterator(cw.root)
-        chart_tags = [e.tag for e in root]
-        for tag in tagnames:
-            assert tag in chart_tags
-
-        assert 'c:catAx' not in chart_tags
-
-    @pytest.mark.lxml_required
-    def test_serialised(self, pie_chart):
-        """Check the serialised file against sample"""
-        cw = PieChartWriter(pie_chart)
-        xml = cw.write()
-        tree = fromstring(xml)
-        chart_schema.assertValid(tree)
-        expected_file = os.path.join(DATADIR, "writer", "expected", "piechart.xml")
-        with open(expected_file) as expected:
-            diff = compare_xml(xml, expected.read())
-            assert diff is None, diff
-
 
 @pytest.fixture
 def line_chart(ws, Reference, Series, LineChart):
