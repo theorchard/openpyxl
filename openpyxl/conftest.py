a1cd3858f0da115ef36f6602751aed0da063738a
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-04-15 23:13:55 +0200

    Make markers global.

--- openpyxl/conftest.py
+++ openpyxl/conftest.py
@@ -24,3 +24,21 @@ def root_xml():
     from openpyxl.xml.functions import Element
     return Element("test")
 
+
+### Markers ###
+
+def pytest_runtest_setup(item):
+    if isinstance(item, item.Function):
+        try:
+            from PIL import Image
+        except ImportError:
+            Image = False
+        if item.get_marker("pil_required") and Image is False:
+            pytest.skip("PIL must be installed")
+        elif item.get_marker("pil_not_installed") and Image:
+            pytest.skip("PIL is installed")
+        elif item.get_marker("not_py33"):
+            pytest.skip("Ordering is not a given in Python 3")
+        elif item.get_marker("lxml_required"):
+            pytest.skip("LXML is required for some features such as schema validation")
+
--- openpyxl/tests/conftest.py
+++ openpyxl/tests/conftest.py
@@ -43,20 +43,3 @@ def datadir():
     from openpyxl.tests.helper import DATADIR
     from py._path.local import LocalPath
     return LocalPath(DATADIR)
-
-### Markers ###
-
-def pytest_runtest_setup(item):
-    if isinstance(item, item.Function):
-        try:
-            from PIL import Image
-        except ImportError:
-            Image = False
-        if item.get_marker("pil_required") and Image is False:
-            pytest.skip("PIL must be installed")
-        elif item.get_marker("pil_not_installed") and Image:
-            pytest.skip("PIL is installed")
-        elif item.get_marker("not_py33"):
-            pytest.skip("Ordering is not a given in Python 3")
-        elif item.get_marker("lxml_required"):
-            pytest.skip("LXML is required for some features such as schema validation")
