a6654b654b7ad4832fb2bba02bde8e9f2d8bc3c4
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-04-15 19:48:11 +0200

    Refactor test subjects.

--- openpyxl/charts/tests/test_reference.py
+++ openpyxl/charts/tests/test_reference.py
@@ -3,65 +3,6 @@
 import pytest
 
 @pytest.fixture
-def Workbook():
-    from openpyxl.workbook import Workbook
-    return Workbook
-
-
-@pytest.fixture
-def Reference():
-    from openpyxl.charts.reference import Reference
-    return Reference
-
-@pytest.fixture
-def ws(Workbook):
-    """Empty worksheet titled 'data'"""
-    wb = Workbook()
-    ws = wb.get_active_sheet()
-    ws.title = 'data'
-    return ws
-
-
-@pytest.fixture
-def ten_row_sheet(ws):
-    """Worksheet with values 0-9 in the first column"""
-    for i in range(10):
-        ws.append([i])
-    return ws
-
-
-@pytest.fixture
-def sheet(ten_row_sheet):
-    ten_row_sheet.title = "reference"
-    return ten_row_sheet
-
-
-@pytest.fixture
-def cell(sheet, Reference):
-    return Reference(sheet, (1, 1))
-
-
-@pytest.fixture
-def cell_range(sheet, Reference):
-    return Reference(sheet, (1, 1), (10, 1))
-
-
-@pytest.fixture()
-def empty_range(sheet, Reference):
-    for i in range(10):
-        sheet.cell(row=i+1, column=2).value = None
-    return Reference(sheet, (1, 2), (10, 2))
-
-
-@pytest.fixture()
-def missing_values(sheet, Reference):
-    vals = [None, None, 1, 2, 3, 4, 5, 6, 7, 8]
-    for idx, val in enumerate(vals, 1):
-        sheet.cell(row=idx, column=3).value = val
-    return Reference(sheet, (1, 3), (10, 3))
-
-
-@pytest.fixture
 def column_of_letters(sheet, Reference):
     for idx, l in enumerate("ABCDEFGHIJ", 1):
         sheet.cell(row=idx, column=2).value = l
