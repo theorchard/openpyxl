d23b02a72d169cd57f2a2012d14c02818ba9595c
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-05 15:11:41 +0200

    Dictionary always serialises to strings to be used directly in XML.

--- openpyxl/styles/tests/test_colors.py
+++ openpyxl/styles/tests/test_colors.py
@@ -20,24 +20,24 @@ class TestColor:
         c = Color(indexed=4)
         assert c.value == 4
         assert c.type == "indexed"
-        assert dict(c) == {'indexed': 4}
+        assert dict(c) == {'indexed': "4"}
 
     def test_auto(self):
         c = Color(auto=1)
         assert c.type is "auto"
         assert c.value is True
-        assert dict(c) == {'auto': True}
+        assert dict(c) == {'auto': "1"}
 
     def test_theme(self):
         c = Color(theme="1")
         assert c.value == 1
         assert c.type == "theme"
-        assert dict(c) ==  {'theme': 1}
+        assert dict(c) ==  {'theme': "1"}
 
     def test_tint(self):
         c = Color(tint=0.5)
         assert c.tint == 0.5
-        assert dict(c) == {'rgb': '00000000', 'tint': 0.5}
+        assert dict(c) == {'rgb': '00000000', 'tint': "0.5"}
 
     def test_highlander(self):
         c = Color(rgb="FFFFFFF", indexed=4, theme=2, auto=False)
