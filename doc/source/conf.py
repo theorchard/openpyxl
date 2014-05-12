30afaf14366b9ec362cbaace179cd0ee76fd7322
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-12 17:49:36 +0200

    Add coverage extension. Can be called using `sphinx-build -b coverage source_dir build_dir; less build_dir/python.txt

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,8 @@ import openpyxl
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.doctest']
+              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.doctest',
+              'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
