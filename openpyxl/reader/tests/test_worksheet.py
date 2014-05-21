f1e4e5857574008cdf1ccf98d82531c417835803
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-21 19:23:49 +0200

    Preliminary unit test for reading column dimensions.

---
+++ openpyxl/reader/tests/test_worksheet.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2010-2014 openpyxl
+
+import py.test
+
+from openpyxl.xml.constants import SHEET_MAIN_NS
+from openpyxl.xml.functions import safe_iterator, fromstring, iterparse
+
+class DummyWorkbook:
+
+    _guess_types = False
+    data_only = False
+
+
+class DummyWorksheet:
+
+    def __init__(self):
+        self.parent = DummyWorkbook()
+        self.column_dimensions = {}
+        self._styles = {}
+
+
+def test_parse_col_dimensions(datadir):
+    from openpyxl.reader.worksheet import WorkSheetParser
+
+    datadir.chdir()
+    ws = DummyWorksheet()
+
+    with open("complex-styles-worksheet.xml") as src:
+        parser = WorkSheetParser(ws, src, {}, {})
+        tree = iterparse(parser.source)
+        for _, tag in tree:
+            cols = safe_iterator(tag, '{%s}col' % SHEET_MAIN_NS)
+            for col in cols:
+                parser.parse_column_dimensions(col)
+    assert ws.column_dimensions.keys() == set(['A', 'C', 'E', 'I', 'G'])
+    assert ws.column_dimensions['A'].width == 31.1640625
