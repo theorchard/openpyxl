636fd296636af1e63e7994d71778dec62b7cb662
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-27 19:20:00 +0200

    Move tests to package.

--- openpyxl/writer/tests/test_worksheet.py
+++ openpyxl/writer/tests/test_worksheet.py
@@ -1,11 +1,18 @@
 from __future__ import absolute_import
 # Copyright (c) 2010-2014 openpyxl
 
-
+import decimal
 from io import BytesIO
+
 import pytest
 
 from openpyxl.xml.functions import XMLGenerator
+from openpyxl import Workbook
+
+from .. strings import create_string_table
+from .. styles import StyleWriter
+from .. workbook import write_workbook
+from .. worksheet import write_worksheet, write_worksheet_rels
 
 from openpyxl.tests.helper import compare_xml
 
@@ -97,3 +104,247 @@ def test_write_lots_cols(out, doc, write_cols, ColumnDimension):
 """
     diff = compare_xml(xml, expected)
     assert diff is None, diff
+
+
+
+def test_write_string(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('F42').value = 'hello'
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+def test_write_hidden_worksheet(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.sheet_state = ws.SHEETSTATE_HIDDEN
+    ws.cell('F42').value = 'hello'
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+def test_write_bool(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('F42').value = False
+    ws.cell('F43').value = True
+    content = write_worksheet(ws, {}, {})
+    with open('sheet1_bool.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+def test_write_formula(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('F1').value = 10
+    ws.cell('F2').value = 32
+    ws.cell('F3').value = '=F1+F2'
+    ws.cell('A4').value = '=A1+A2+A3'
+    ws.formula_attributes['A4'] = {'t': 'shared', 'ref': 'A4:C4', 'si': '0'}
+    ws.cell('B4').value = '='
+    ws.formula_attributes['B4'] = {'t': 'shared', 'si': '0'}
+    ws.cell('C4').value = '='
+    ws.formula_attributes['C4'] = {'t': 'shared', 'si': '0'}
+    content = write_worksheet(ws, {}, {})
+    with open('sheet1_formula.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+# check style tests
+def test_write_style(datadir):
+    datadir.chdir()
+    wb = Workbook(guess_types=True)
+    ws = wb.create_sheet()
+    ws.cell('F1').value = '13%'
+    ws._styles['F'] = ws._styles['F1']
+    styles = StyleWriter(wb).styles
+    content = write_worksheet(ws, {}, styles)
+    with open('sheet1_style.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+def test_write_height(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('F1').value = 10
+    ws.row_dimensions[ws.cell('F1').row].height = 30
+    content = write_worksheet(ws, {}, {})
+    with open('sheet1_height.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+def test_write_hyperlink(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('A1').value = "test"
+    ws.cell('A1').hyperlink = "http://test.com"
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1_hyperlink.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+def test_write_hyperlink_rels(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    assert 0 == len(ws.relationships)
+    ws.cell('A1').value = "test"
+    ws.cell('A1').hyperlink = "http://test.com/"
+    assert 1 == len(ws.relationships)
+    ws.cell('A2').value = "test"
+    ws.cell('A2').hyperlink = "http://test2.com/"
+    assert 2 == len(ws.relationships)
+    content = write_worksheet_rels(ws, 1, 1)
+    with open('sheet1_hyperlink.xml.rels') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+@pytest.mark.xfail
+@pytest.mark.pil_required
+def test_write_hyperlink_image_rels(Workbook, Image, datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('A1').value = "test"
+    ws.cell('A1').hyperlink = "http://test.com/"
+    i = Image( "plain.png")
+    ws.add_image(i)
+    raise ValueError("Resulting file is invalid")
+    # TODO write integration test with duplicate relation ids then fix
+
+
+def test_hyperlink_value():
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('A1').hyperlink = "http://test.com"
+    assert "http://test.com" == ws.cell('A1').value
+    ws.cell('A1').value = "test"
+    assert "test" == ws.cell('A1').value
+
+
+def test_write_auto_filter(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.worksheets[0]
+    ws.cell('F42').value = 'hello'
+    ws.auto_filter.ref = 'A1:F1'
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1_auto_filter.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None
+
+    content = write_workbook(wb)
+    with open('workbook_auto_filter.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+def test_write_auto_filter_filter_column(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.worksheets[0]
+    ws.cell('F42').value = 'hello'
+    ws.auto_filter.ref = 'A1:F1'
+    ws.auto_filter.add_filter_column(0, ["0"], blank=True)
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1_auto_filter_filter_column.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None
+
+    content = write_workbook(wb)
+    with open('workbook_auto_filter.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+def test_write_auto_filter_sort_condition(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.worksheets[0]
+    ws.cell('A1').value = 'header'
+    ws.cell('A2').value = 1
+    ws.cell('A3').value = 0
+    ws.auto_filter.ref = 'A2:A3'
+    ws.auto_filter.add_sort_condition('A2:A3', descending=True)
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1_auto_filter_sort_condition.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None
+
+    content = write_workbook(wb)
+    with open('workbook_auto_filter.xml') as expected:
+        diff = compare_xml(content, expected.read())
+
+def test_freeze_panes_horiz(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('F42').value = 'hello'
+    ws.freeze_panes = 'A4'
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1_freeze_panes_horiz.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+def test_freeze_panes_vert(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('F42').value = 'hello'
+    ws.freeze_panes = 'D1'
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1_freeze_panes_vert.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+def test_freeze_panes_both(datadir):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('F42').value = 'hello'
+    ws.freeze_panes = 'D4'
+    strings = create_string_table(wb)
+    content = write_worksheet(ws, strings, {})
+    with open('sheet1_freeze_panes_both.xml') as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
+
+
+@pytest.mark.parametrize("value, filename",
+                         [
+                             (9781231231230, 'long_number.xml'),
+                             (decimal.Decimal('3.14'), 'decimal.xml'),
+                             (1234567890, 'short_number.xml')
+                         ])
+def test_numbers(datadir, value, filename):
+    datadir.chdir()
+    wb = Workbook()
+    ws = wb.create_sheet()
+    ws.cell('A1').value = value
+    content = write_worksheet(ws, {}, {})
+    with open(filename) as expected:
+        diff = compare_xml(content, expected.read())
+        assert diff is None, diff
