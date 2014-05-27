737e7643c0fb67197b0f751649ae280c131b7e18
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-27 19:46:30 +0200

    Experimental lxml incremental writer.

---
+++ openpyxl/writer/lxml_worksheet.py
@@ -0,0 +1,90 @@
+from __future__ import absolute_import
+# Copyright (c) 2010-2014 openpyxl
+
+# Experimental writer of worksheet data using lxml incremental API
+
+from lxml.etree import xmlfile, Element
+
+from openpyxl.compat import iterkeys, itervalues, safe_string
+from .worksheet import row_sort
+
+
+def write_worksheet_data(doc, worksheet, string_table, style_table):
+    """Write worksheet data to xml."""
+
+    # Ensure a blank cell exists if it has a style
+    for styleCoord in iterkeys(worksheet._styles):
+        if isinstance(styleCoord, str) and COORD_RE.search(styleCoord):
+            worksheet.cell(styleCoord)
+
+    # create rows of cells
+    cells_by_row = {}
+    for cell in itervalues(worksheet._cells):
+        cells_by_row.setdefault(cell.row, []).append(cell)
+
+    with xmlfile(doc) as xf:
+        with xf.element("sheetData"):
+            for row_idx in sorted(cells_by_row):
+                # row meta data
+                row_dimension = worksheet.row_dimensions[row_idx]
+                row_dimension.style = worksheet._styles.get(row_idx)
+                attrs = {'r': '%d' % row_idx,
+                         'spans': '1:%d' % worksheet.max_column}
+                attrs.update(dict(row_dimension))
+
+                with xf.element("row", attrs):
+
+                    row_cells = cells_by_row[row_idx]
+                    for cell in sorted(row_cells, key=row_sort):
+                        write_cell(xf, worksheet, cell, string_table)
+
+                #end_tag(doc, 'row')
+            #end_tag(doc, 'sheetData')
+
+
+def write_cell(xf, worksheet, cell, string_table):
+    coordinate = cell.coordinate
+    attributes = {'r': coordinate}
+    cell_style = worksheet._styles.get(coordinate)
+    if cell_style is not None:
+        attributes['s'] = '%d' % cell_style
+
+    if cell.data_type != cell.TYPE_FORMULA:
+        attributes['t'] = cell.data_type
+
+    value = cell.internal_value
+    if value in ('', None):
+        xf.element("c", attributes)
+    else:
+        with xf.element('c', attributes):
+            if cell.data_type == cell.TYPE_STRING:
+                #tag(doc, 'v', body='%s' % string_table.index(value))
+                el = Element('v', attributes)
+                el.text = '%d' % string_table.index(value)
+                xf.write(el)
+                el = None
+            elif cell.data_type == cell.TYPE_FORMULA:
+                shared_formula = worksheet.formula_attributes.get(coordinate)
+                attr = {}
+                if shared_formula is not None:
+                    attr = shared_formula
+                    if ('t' in attr
+                        and attr['t'] == 'shared'
+                        and 'ref' not in attr):
+                        # Don't write body for shared formula
+                        value = None
+                el = Element('f', attr)
+                if value is not None:
+                    el.text = value[1:]
+                    xf.write(el)
+                    el = None
+                #tag(doc, 'v')
+                xf.element("v")
+            else:
+                #tag(doc, 'v', body=safe_string(value))
+                el = Element('v')
+                el.text = safe_string(value)
+                xf.write(el)
+            #else:
+                #tag(doc, 'v', body=value)
+            #end_tag(doc, 'c')
---
+++ openpyxl/writer/tests/test_lxml.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+# Copyright (c) 2010-2014 openpyxl
+
+# stdlib
+import decimal
+from io import BytesIO
+
+# package
+from openpyxl import Workbook
+from lxml.etree import xmlfile
+
+# test imports
+import pytest
+from openpyxl.tests.helper import compare_xml
+
+
+@pytest.fixture
+def out():
+    return BytesIO()
+
+
+@pytest.mark.parametrize("value, expected",
+                         [
+                             (9781231231230, """<c t="n" r="A1"><v>9781231231230</v></c>"""),
+                             (decimal.Decimal('3.14'), """<c t="n" r="A1"><v>3.14</v></c>"""),
+                             (1234567890, """<c t="n" r="A1"><v>1234567890</v></c>""")
+                         ])
+def test_write_cell(out, value, expected):
+    from .. lxml_worksheet import write_cell
+
+    wb = Workbook()
+    ws = wb.active
+    ws['A1'] = value
+    with xmlfile(out) as xf:
+        write_cell(xf, ws, ws['A1'], [])
+    xml = out.getvalue()
+    diff = compare_xml(xml, expected)
+    assert diff is None, diff
+
+
+def test_write_sheetdata(out):
+    from .. lxml_worksheet import write_worksheet_data
+
+    wb = Workbook()
+    ws = wb.active
+    ws['A1'] = 10
+    write_worksheet_data(out, ws, [], None)
+    xml = out.getvalue()
+    expected = """<sheetData><row r="1" spans="1:1"><c t="n" r="A1"><v>10</v></c></row></sheetData>"""
+    diff = compare_xml(xml, expected)
+    assert diff is None, diff
