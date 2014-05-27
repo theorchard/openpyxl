d158ea0d369eaa4ff6c61681b562f03e79e5f4ca
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-27 19:47:20 +0200

    Some profiling scripts to compare writer implementations.

---
+++ openpyxl/benchmarks/profiling.py
@@ -0,0 +1,34 @@
+from io import BytesIO
+
+from openpyxl import Workbook
+from openpyxl.xml.functions import XMLGenerator
+
+
+def make_worksheet():
+    wb = Workbook()
+    ws = wb.active
+    for i in range(1000):
+        ws.append(list(range(100)))
+    return ws
+
+
+def sax_writer(ws=None):
+    from openpyxl.writer.worksheet import write_worksheet_data
+    if ws is None:
+        ws = make_worksheet()
+    out = BytesIO()
+    doc = XMLGenerator(out)
+    write_worksheet_data(doc, ws, [], [])
+    #with open("sax_writer.xml", "wb") as dump:
+        #dump.write(out.getvalue())
+
+
+def lxml_writer(ws=None):
+    from openpyxl.writer.lxml_worksheet import write_worksheet_data
+    if ws is None:
+        ws = make_worksheet()
+
+    out = BytesIO()
+    write_worksheet_data(out, ws, [], [])
+    #with open("lxl_writer.xml", "wb") as dump:
+        #dump.write(out.getvalue())
