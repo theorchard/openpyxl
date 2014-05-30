1dd5da556066772e3dd71d71061f6a569f615bcd
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-30 11:59:45 +0200

    Use the same file to test sharedStrings reading and writing.

--- openpyxl/tests/test_strings.py
+++ openpyxl/tests/test_strings.py
@@ -48,8 +48,8 @@ def test_formatted_string_table(datadir):
 def test_write_string_table(datadir):
     from openpyxl.writer.strings import write_string_table
 
-    datadir.join("writer, expected").chdir()
-    table = ['hello', 'world', 'nice']
+    datadir.join("reader").chdir()
+    table = ['This is cell A1 in Sheet 1', 'This is cell G5']
     content = write_string_table(table)
     with open('sharedStrings.xml') as expected:
         diff = compare_xml(content, expected.read())
