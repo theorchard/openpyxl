e3e44689314d9f9f48fa569e0494ffd18bcc960b
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-05-30 12:32:36 +0200

    Use fixtures.

--- openpyxl/tests/test_meta.py
+++ openpyxl/tests/test_meta.py
@@ -1,50 +1,28 @@
 # Copyright (c) 2010-2014 openpyxl
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-# @license: http://www.opensource.org/licenses/mit-license.php
-# @author: see AUTHORS file
 
 # Python stdlib imports
-import os.path
 
 # package imports
-from openpyxl.tests.helper import DATADIR, compare_xml
+from openpyxl.tests.helper import compare_xml
 from openpyxl.writer.workbook import write_content_types, write_root_rels
 from openpyxl.workbook import Workbook
 
 
-def test_write_content_types():
+def test_write_content_types(datadir):
+    datadir.join("writer").chdir()
     wb = Workbook()
     wb.create_sheet()
     wb.create_sheet()
     content = write_content_types(wb)
-    reference_file = os.path.join(DATADIR, 'writer', '[Content_Types].xml')
-    with open(reference_file) as expected:
+    with open('[Content_Types].xml') as expected:
         diff = compare_xml(content, expected.read())
         assert diff is None, diff
 
 
-def test_write_root_rels():
+def test_write_root_rels(datadir):
+    datadir.join("writer").chdir()
     wb = Workbook()
     content = write_root_rels(wb)
-    reference_file = os.path.join(DATADIR, 'writer', '.rels')
-    with open(reference_file) as expected:
+    with open('.rels') as expected:
         diff = compare_xml(content, expected.read())
         assert diff is None, diff
