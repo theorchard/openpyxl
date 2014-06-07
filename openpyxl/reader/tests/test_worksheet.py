5d46d783db5c5a27f00cad36fb3a8a72f83768e7
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-06-07 19:43:26 +0200

    Explicitly use lxml.etree.iterparse to simplify parsing for only one tag type.

--- openpyxl/reader/tests/test_worksheet.py
+++ openpyxl/reader/tests/test_worksheet.py
@@ -3,7 +3,7 @@
 import pytest
 
 from openpyxl.xml.constants import SHEET_MAIN_NS
-from openpyxl.xml.functions import safe_iterator, fromstring, iterparse
+from lxml.etree import iterparse
 
 
 @pytest.fixture
@@ -34,16 +34,27 @@ def test_col_width(datadir, Worksheet, WorkSheetParser):
     ws = Worksheet
     parser = WorkSheetParser
 
-    with open("complex-styles-worksheet.xml") as src:
-        tree = iterparse(src)
-        for _, tag in tree:
-            cols = safe_iterator(tag, '{%s}col' % SHEET_MAIN_NS)
-            for col in cols:
-                parser.parse_column_dimensions(col)
+    with open("complex-styles-worksheet.xml", "rb") as src:
+        cols = iterparse(src, tag='{%s}col' % SHEET_MAIN_NS)
+        for _, col in cols:
+            parser.parse_column_dimensions(col)
     assert set(ws.column_dimensions.keys()) == set(['A', 'C', 'E', 'I', 'G'])
     assert dict(ws.column_dimensions['A']) == {'max': '1', 'min': '1',
                                                'customWidth': '1',
                                                'width': '31.1640625'}
+
+
+def test_hidden_col(datadir, Worksheet, WorkSheetParser):
+    datadir.chdir()
+    ws = Worksheet
+    parser = WorkSheetParser
+
+    with open("hidden_rows_cols.xml", "rb") as src:
+        cols = iterparse(src, tag='{%s}col' % SHEET_MAIN_NS)
+        for _, col in cols:
+            parser.parse_column_dimensions(col)
+    assert 'D' in ws.column_dimensions
+    assert dict(ws.column_dimensions['D']) == {'customWidth': '1', 'hidden': '1', 'max': '4', 'min': '4'}
 
 
 def test_sheet_protection(datadir, Worksheet, WorkSheetParser):
@@ -51,12 +62,10 @@ def test_sheet_protection(datadir, Worksheet, WorkSheetParser):
     ws = Worksheet
     parser = WorkSheetParser
 
-    with open("protected_sheet.xml") as src:
-        tree = iterparse(src)
+    with open("protected_sheet.xml", "rb") as src:
+        tree = iterparse(src, tag='{%s}sheetProtection' % SHEET_MAIN_NS)
         for _, tag in tree:
-            prot = safe_iterator(tag, '{%s}sheetProtection' % SHEET_MAIN_NS)
-            for el in prot:
-                parser.parse_sheet_protection(el)
+            parser.parse_sheet_protection(tag)
     assert dict(ws.protection) == {
         'autoFilter': '1', 'deleteColumns': '1',
         'deleteRows': '1', 'formatCells': '1', 'formatColumns': '1', 'formatRows':
