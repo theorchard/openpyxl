543d7f68788c648379ce5164bb1be519a02310e6
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-08-04 12:51:55 +0200

    Additional unit tests

--- openpyxl/writer/tests/test_dump.py
+++ openpyxl/writer/tests/test_dump.py
@@ -3,12 +3,20 @@ from __future__ import absolute_import
 
 import pytest
 
+from openpyxl.compat import file
+from openpyxl.tests.helper import compare_xml
+
+class DummyLocalData:
+
+    pass
+
 
 class DummyWorkbook:
 
     def __init__(self):
         self.shared_strings = []
         self.shared_styles = []
+        self._local_data = DummyLocalData()
 
     def get_sheet_names(self):
         return []
@@ -30,3 +38,47 @@ def test_ctor(DumpWorksheet):
     assert hasattr(ws, '_fileobj_content_name')
     assert hasattr(ws, '_fileobj_name')
     assert ws._comments == []
+
+
+def test_cache(DumpWorksheet):
+    ws = DumpWorksheet
+    assert ws._descriptors_cache == {}
+
+
+def test_filename(DumpWorksheet):
+    ws = DumpWorksheet
+    assert ws.filename == ws._fileobj_name
+
+
+def test_get_temporary_file(DumpWorksheet):
+    ws = DumpWorksheet
+    fobj = ws.get_temporary_file(ws.filename)
+    assert fobj.mode == "r+"
+
+
+def test_dimensions(DumpWorksheet):
+    ws = DumpWorksheet
+    assert ws.get_dimensions() == 'A1'
+
+
+def test_write_header(DumpWorksheet):
+    ws = DumpWorksheet
+    doc = ws.write_header()
+    doc._flush()
+    xml = open(ws._fileobj_header_name).read()
+    xml += "</worksheet>"
+    expected = """<worksheet xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+    <sheetPr>
+      <outlinePr summaryRight="1" summaryBelow="1"/>
+    </sheetPr>
+    <dimension ref="A1:A1"/>
+    <sheetViews>
+      <sheetView workbookViewId="0">
+        <selection sqref="A1" activeCell="A1"/>
+      </sheetView>
+    </sheetViews>
+    <sheetFormatPr baseColWidth="10" defaultRowHeight="15"/>
+    </worksheet>
+    """
+    diff = compare_xml(xml, expected)
+    assert diff is None, diff
