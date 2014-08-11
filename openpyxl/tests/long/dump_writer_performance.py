0837a0c5e6e8b449d6bac767d3d4df2f82610ae1
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-08-11 17:26:07 +0200

    Python 3 compatible and use both modes.

--- openpyxl/tests/long/dump_writer_performance.py
+++ openpyxl/tests/long/dump_writer_performance.py
@@ -3,14 +3,17 @@ from openpyxl.compat import range
 
 import tempfile
 
+import pytest
 
-def test_large_append():
-    wb = openpyxl.Workbook(optimized_write=True)
+@pytest.mark.parametrize("mode", (True, False))
+def test_large_append(mode):
+    print("Using write only mode {0}".format(mode))
+    wb = openpyxl.Workbook(optimized_write=mode)
     ws = wb.create_sheet()
     row = ('this is some text', 3.14)
     total_rows = int(2e4)
     for idx in range(total_rows):
         if not idx % 10000:
-            print "%.2f%%" % (100 * (float(idx) / float(total_rows)))
+            print("%.2f%%" % (100 * (float(idx) / float(total_rows))))
         ws.append(row)
     wb.save(tempfile.TemporaryFile(mode='wb'))
