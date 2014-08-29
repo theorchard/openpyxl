da1a79836f708f94422a4b0b654914f158ccc81e
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-08-29 10:08:37 +0200

    More explicitly demonstrate that tuples of columns are returned.

--- openpyxl/worksheet/tests/test_worksheet.py
+++ openpyxl/worksheet/tests/test_worksheet.py
@@ -274,13 +274,20 @@ class TestWorksheet(object):
 
 
     def test_cols(self):
-
         ws = Worksheet(self.wb)
 
         ws.cell('A1').value = 'first'
         ws.cell('C9').value = 'last'
+        expected = [
+            ('A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9'),
+            ('B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9'),
+            ('C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'),
+
+        ]
 
         cols = ws.columns
+        for col, coord in zip(cols, expected):
+            assert tuple(c.coordinate for c in col) == coord
 
         assert len(cols) == 3
 
