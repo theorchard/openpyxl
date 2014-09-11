4351ee9a6b28e6b539a3941fca5a4c59334e651f
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-09-11 10:35:42 +0200

    Add workbook fixture and convert single methof test classes to functions.

--- openpyxl/tests/test_iter.py
+++ openpyxl/tests/test_iter.py
@@ -47,6 +47,13 @@ def test_get_highest_row(datadir):
     assert 30 == max_row
 
 
+@pytest.fixture
+def sample_workbook(datadir):
+    datadir.join("genuine").chdir()
+    wb = load_workbook(filename="empty.xlsx", use_iterators=True,
+                       data_only=True)
+    return wb
+
 
 class TestWorksheet(object):
 
@@ -57,176 +64,161 @@ class TestWorksheet(object):
                              use_iterators=True,
                              data_only=data_only)
 
-    def test_getitem(self):
-        wb = self._open_wb()
-        ws = wb['Sheet1 - Text']
-        assert list(ws.iter_rows("A1"))[0][0] == ws['A1']
-        assert list(ws.iter_rows("A1:D30")) == list(ws["A1:D30"])
-        assert list(ws.iter_rows("A1:D30")) == list(ws["A1":"D30"])
+def test_getitem(sample_workbook):
+    wb = sample_workbook
+    ws = wb['Sheet1 - Text']
+    assert list(ws.iter_rows("A1"))[0][0] == ws['A1']
+    assert list(ws.iter_rows("A1:D30")) == list(ws["A1:D30"])
+    assert list(ws.iter_rows("A1:D30")) == list(ws["A1":"D30"])
 
-        ws = wb['Sheet2 - Numbers']
-        assert ws['A1'] is None
+    ws = wb['Sheet2 - Numbers']
+    assert ws['A1'] is None
 
 
-class TestDims(TestWorksheet):
-    expected = [
-        ("Sheet1 - Text", 'A1:G5'),
-        ("Sheet2 - Numbers", 'D1:AA30'),
-        ("Sheet3 - Formulas", 'D2:D2'),
-        ("Sheet4 - Dates", 'A1:C1')
+expected = [
+    ("Sheet1 - Text", 'A1:G5'),
+    ("Sheet2 - Numbers", 'D1:AA30'),
+    ("Sheet3 - Formulas", 'D2:D2'),
+    ("Sheet4 - Dates", 'A1:C1')
                  ]
-    @pytest.mark.parametrize("sheetname, dims", expected)
-    def test_get_dimensions(self, sheetname, dims):
-        wb = self._open_wb()
-        ws = wb[sheetname]
-        assert ws.dimensions == dims
+@pytest.mark.parametrize("sheetname, dims", expected)
+def test_get_dimensions(sample_workbook, sheetname, dims):
+    wb = sample_workbook
+    ws = wb[sheetname]
+    assert ws.dimensions == dims
 
-    expected = [
-        ("Sheet1 - Text", 7),
-        ("Sheet2 - Numbers", 27),
-        ("Sheet3 - Formulas", 4),
-        ("Sheet4 - Dates", 3)
-                 ]
-    @pytest.mark.parametrize("sheetname, col", expected)
-    def test_get_highest_column_iter(self, sheetname, col):
-        wb = self._open_wb()
-        ws = wb[sheetname]
-        assert ws.get_highest_column() == col
+expected = [
+    ("Sheet1 - Text", 7),
+    ("Sheet2 - Numbers", 27),
+    ("Sheet3 - Formulas", 4),
+    ("Sheet4 - Dates", 3)
+             ]
+@pytest.mark.parametrize("sheetname, col", expected)
+def test_get_highest_column_iter(sample_workbook, sheetname, col):
+    wb = sample_workbook
+    ws = wb[sheetname]
+    assert ws.get_highest_column() == col
 
 
-class TestText(TestWorksheet):
-    sheet_name = 'Sheet1 - Text'
-    expected = [['This is cell A1 in Sheet 1', None, None, None, None, None, None],
-                [None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, 'This is cell G5'], ]
-    def test_read_fast_integrated(self):
-        wb = self._open_wb()
-        ws = wb.get_sheet_by_name(name = self.sheet_name)
-        for row, expected_row in zip(ws.iter_rows(), self.expected):
-            row_values = [x.value for x in row]
-            assert row_values == expected_row
+expected = [['This is cell A1 in Sheet 1', None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, 'This is cell G5'], ]
+def test_read_fast_integrated(sample_workbook):
+    wb = sample_workbook
+    ws = wb.get_sheet_by_name('Sheet1 - Text')
+    for row, expected_row in zip(ws.iter_rows(), self.expected):
+        row_values = [x.value for x in row]
+        assert row_values == expected_row
 
-    def test_read_single_cell_range(self):
-        wb = self._open_wb()
-        ws = wb.get_sheet_by_name(name = self.sheet_name)
-        assert 'This is cell A1 in Sheet 1' == list(ws.iter_rows('A1'))[0][0].value
 
-class TestIntegers(TestWorksheet):
+def test_read_single_cell_range(sample_workbook):
+    wb = sample_workbook
+    ws = wb.get_sheet_by_name('Sheet1 - Text')
+    assert 'This is cell A1 in Sheet 1' == list(ws.iter_rows('A1'))[0][0].value
 
+
+def test_read_fast_integrated(sample_workbook):
+    wb = sample_workbook
     sheet_name = 'Sheet2 - Numbers'
     expected = [[x + 1] for x in range(30)]
     query_range = 'D1:D30'
-
-    def test_read_fast_integrated(self):
-        wb = self._open_wb()
-        ws = wb.get_sheet_by_name(name = self.sheet_name)
-        for row, expected_row in zip(ws.iter_rows(self.query_range), self.expected):
-            row_values = [x.value for x in row]
-            assert row_values == expected_row
+    ws = wb.get_sheet_by_name(name = sheet_name)
+    for row, expected_row in zip(ws.iter_rows(query_range), expected):
+        row_values = [x.value for x in row]
+        assert row_values == expected_row
 
 
-class TestFloats(TestWorksheet):
-
+def test_read_fast_integrated(sample_workbook):
+    wb = sample_workbook
     sheet_name = 'Sheet2 - Numbers'
     query_range = 'K1:K30'
     expected = expected = [[(x + 1) / 100.0] for x in range(30)]
-
-    def test_read_fast_integrated(self):
-        wb = self._open_wb()
-        ws = wb.get_sheet_by_name(name = self.sheet_name)
-        for row, expected_row in zip(ws.iter_rows(self.query_range), self.expected):
-            row_values = [x.value for x in row]
-            assert row_values == expected_row
+    ws = wb.get_sheet_by_name(name = sheet_name)
+    for row, expected_row in zip(ws.iter_rows(query_range), expected):
+        row_values = [x.value for x in row]
+        assert row_values == expected_row
 
 
-class TestDates(TestWorksheet):
-
-    sheet_name = 'Sheet4 - Dates'
-
-    @pytest.mark.parametrize("cell, value",
-        [
-        ("A1", datetime.datetime(1973, 5, 20)),
-        ("C1", datetime.datetime(1973, 5, 20, 9, 15, 2))
-        ]
-        )
-    def test_read_single_cell_date(self, cell, value):
-        wb = self._open_wb()
-        ws = wb.get_sheet_by_name(name = self.sheet_name)
-        rows = ws.iter_rows(cell)
-        cell = list(rows)[0][0]
-        assert cell.value == value
-
-class TestFormula(TestWorksheet):
-
-    @pytest.mark.parametrize("data_only, expected",
-        [
-        (True, 5),
-        (False, "='Sheet2 - Numbers'!D5")
-        ]
-        )
-    def test_read_single_cell_formula(self, data_only, expected):
-        wb = self._open_wb(data_only)
-        ws = wb.get_sheet_by_name("Sheet3 - Formulas")
-        rows = ws.iter_rows("D2")
-        cell = list(rows)[0][0]
-        assert ws.parent.data_only == data_only
-        assert cell.value == expected
+@pytest.mark.parametrize("cell, value",
+    [
+    ("A1", datetime.datetime(1973, 5, 20)),
+    ("C1", datetime.datetime(1973, 5, 20, 9, 15, 2))
+    ]
+    )
+def test_read_single_cell_date(sample_workbook, cell, value):
+    wb = sample_workbook
+    ws = wb.get_sheet_by_name('Sheet4 - Dates')
+    rows = ws.iter_rows(cell)
+    cell = list(rows)[0][0]
+    assert cell.value == value
 
 
-class TestBoolean(TestWorksheet):
+@pytest.mark.parametrize("data_only, expected",
+    [
+    (True, 5),
+    (False, "='Sheet2 - Numbers'!D5")
+    ]
+    )
+def test_read_single_cell_formula(datadir, data_only, expected):
+    datadir.join("genuine").chdir()
+    wb = load_workbook("empty.xlsx", read_only=True, data_only=data_only)
+    ws = wb.get_sheet_by_name("Sheet3 - Formulas")
+    rows = ws.iter_rows("D2")
+    cell = list(rows)[0][0]
+    assert ws.parent.data_only == data_only
+    assert cell.value == expected
 
-    @pytest.mark.parametrize("cell, expected",
-        [
-        ("G9", True),
-        ("G10", False)
-        ]
-        )
-    def test_read_boolean(self, cell, expected):
-        wb = self._open_wb()
-        ws = wb["Sheet2 - Numbers"]
-        row = list(ws.iter_rows(cell))
-        assert row[0][0].coordinate == cell
-        assert row[0][0].data_type == 'b'
-        assert row[0][0].value == expected
+
+@pytest.mark.parametrize("cell, expected",
+    [
+    ("G9", True),
+    ("G10", False)
+    ]
+    )
+def test_read_boolean(sample_workbook, cell, expected):
+    wb = sample_workbook
+    ws = wb["Sheet2 - Numbers"]
+    row = list(ws.iter_rows(cell))
+    assert row[0][0].coordinate == cell
+    assert row[0][0].data_type == 'b'
+    assert row[0][0].value == expected
 
 
-class TestStyleIter(object):
+def test_read_style_iter():
+    '''
+    Test if cell styles are read properly in iter mode.
+    '''
+    import tempfile
+    from openpyxl import Workbook
+    from openpyxl.styles import Style, Font
 
-    def test_read_style_iter(self):
-        '''
-        Test if cell styles are read properly in iter mode.
-        '''
-        import tempfile
-        from openpyxl import Workbook
-        from openpyxl.styles import Style, Font
+    FONT_NAME = "Times New Roman"
+    FONT_SIZE = 15
 
-        FONT_NAME = "Times New Roman"
-        FONT_SIZE = 15
+    wb = Workbook()
+    ws = wb.worksheets[0]
+    cell = ws.cell('A1')
+    cell.style = Style(font=Font(name=FONT_NAME, size=FONT_SIZE))
 
-        wb = Workbook()
-        ws = wb.worksheets[0]
-        cell = ws.cell('A1')
-        cell.style = Style(font=Font(name=FONT_NAME, size=FONT_SIZE))
+    xlsx_file = tempfile.NamedTemporaryFile()
+    wb.save(xlsx_file)
 
-        xlsx_file = tempfile.NamedTemporaryFile()
-        wb.save(xlsx_file)
+    # Passes as of 1.6.1
+    wb_regular = load_workbook(xlsx_file)
+    ws_regular = wb_regular.worksheets[0]
+    cell_style_regular = ws_regular.cell('A1').style
+    assert cell_style_regular.font.name == FONT_NAME
+    assert cell_style_regular.font.size == FONT_SIZE
 
-        # Passes as of 1.6.1
-        wb_regular = load_workbook(xlsx_file)
-        ws_regular = wb_regular.worksheets[0]
-        cell_style_regular = ws_regular.cell('A1').style
-        assert cell_style_regular.font.name == FONT_NAME
-        assert cell_style_regular.font.size == FONT_SIZE
+    # Fails as of 1.6.1
+    # perhaps not correct
+    # but would work if style_table was not ignored and styles
+    # would still be present in ws_iter._styles
+    wb_iter = load_workbook(xlsx_file, use_iterators=True)
+    ws_iter = wb_iter.worksheets[0]
+    cell = ws_iter['A1']
 
-        # Fails as of 1.6.1
-        # perhaps not correct
-        # but would work if style_table was not ignored and styles
-        # would still be present in ws_iter._styles
-        wb_iter = load_workbook(xlsx_file, use_iterators=True)
-        ws_iter = wb_iter.worksheets[0]
-        cell = ws_iter['A1']
-
-        assert cell.style.font.name == FONT_NAME
-        assert cell.style.font.size == FONT_SIZE
+    assert cell.style.font.name == FONT_NAME
+    assert cell.style.font.size == FONT_SIZE
