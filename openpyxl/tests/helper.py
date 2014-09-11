20622bcbae0e7efcd7984066c3959a92707924e0
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-09-11 10:38:07 +0200

    Remove unused functions.

--- openpyxl/tests/helper.py
+++ openpyxl/tests/helper.py
@@ -2,28 +2,7 @@ from __future__ import absolute_import
 # Copyright (c) 2010-2014 openpyxl
 
 # Python stdlib imports
-from io import BytesIO
-import os.path
-from sys import version_info
 from lxml.doctestcompare import LXMLOutputChecker, PARSE_XML
-
-# package imports
-
-# constants
-DATADIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
-
-def get_xml(xml_node):
-
-    io = BytesIO()
-    if version_info[0] >= 3 and version_info[1] >= 2:
-        ElementTree(xml_node).write(io, encoding='UTF-8', xml_declaration=False)
-        ret = str(io.getvalue(), 'utf-8')
-        ret = ret.replace('utf-8', 'UTF-8', 1)
-    else:
-        ElementTree(xml_node).write(io, encoding='UTF-8')
-        ret = io.getvalue()
-    io.close()
-    return ret.replace('\n', '')
 
 
 def compare_xml(generated, expected):
