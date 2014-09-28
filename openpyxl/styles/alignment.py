235ec58b437ea9b9e1fea687128dd5938ba4a86f
Elias Rabel <ylikx.0@gmail.com>
2014-09-28 17:25:51 +0200

    fixed Python 2.6 problem with xmlfile, Python 3.4 has problems

---
+++ openpyxl/styles/alignment.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import
+# Copyright (c) 2010-2014 openpyxl
+
+from openpyxl.descriptors import Set, Bool, MinMax, Min, Alias
+
+from .hashable import HashableObject
+
+horizontal_alignments = (
+    "general", "left", "center", "right", "fill", "justify", "centerContinuous",
+    "distributed", None
+)
+vertical_aligments = (
+    "top", "center", "bottom", "justify", "distributed", None
+)
+
+class Alignment(HashableObject):
+    """Alignment options for use in styles."""
+
+    __fields__ = ('horizontal',
+                  'vertical',
+                  'textRotation',
+                  'wrapText',
+                  'shrinkToFit',
+                  'indent',
+                  'relativeIndent',
+                  'justifyLastLine',
+                  'readingOrder',
+                  )
+    horizontal = Set(values=horizontal_alignments)
+    vertical = Set(values=vertical_aligments)
+    textRotation = Set(values=range(181))
+    textRotation.values.add(255)
+    text_rotation = Alias('textRotation')
+    wrapText = Bool()
+    wrap_text = Alias('wrapText')
+    shrinkToFit = Bool()
+    shrink_to_fit = Alias('shrinkToFit')
+    indent = Min(min=0)
+    relativeIndent = Min(min=0)
+    justifyLastLine = Bool()
+    readingOrder = Min(min=0)
+
+    def __init__(self, horizontal='general', vertical='bottom',
+                 textRotation=0, wrapText=False, shrinkToFit=False, indent=0, relativeIndent=0,
+                 justifyLastLine=False, readingOrder=0, text_rotation=None,
+                 wrap_text=None, shrink_to_fit=None) :
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.indent = indent
+        self.relativeIndent = relativeIndent
+        self.justifyLastLine = justifyLastLine
+        self.readingOrder = readingOrder
+        if text_rotation is not None:
+            textRotation = text_rotation
+        if textRotation is not None:
+            self.textRotation = int(textRotation)
+        if wrap_text is not None:
+            wrapText = wrap_text
+        self.wrapText = wrapText
+        if shrink_to_fit is not None:
+            shrinkToFit = shrink_to_fit
+        self.shrinkToFit = shrinkToFit
---
+++ openpyxl/styles/fonts.py
@@ -0,0 +1,93 @@
+from __future__ import absolute_import
+# Copyright (c) 2010-2014 openpyxl
+
+
+from openpyxl.descriptors import Float, Integer, Set, Bool, String, Alias, MinMax, NoneSet
+from .hashable import HashableObject
+from .colors import ColorDescriptor, BLACK
+
+
+class Font(HashableObject):
+    """Font options used in styles."""
+
+    spec = """18.8.22, p.3930"""
+
+    UNDERLINE_DOUBLE = 'double'
+    UNDERLINE_DOUBLE_ACCOUNTING = 'doubleAccounting'
+    UNDERLINE_SINGLE = 'single'
+    UNDERLINE_SINGLE_ACCOUNTING = 'singleAccounting'
+
+
+    name = String()
+    charset = Integer(allow_none=True)
+    family = MinMax(min=0, max=14)
+    sz = Float()
+    size = Alias("sz")
+    b = Bool()
+    bold = Alias("b")
+    i = Bool()
+    italic = Alias("i")
+    strike = Bool()
+    strikethrough = Alias("strike")
+    outline = Bool()
+    shadow = Bool()
+    condense = Bool()
+    extend = Bool()
+    u = NoneSet(values=(
+        UNDERLINE_DOUBLE,
+        UNDERLINE_DOUBLE_ACCOUNTING,
+        UNDERLINE_SINGLE,
+        UNDERLINE_SINGLE_ACCOUNTING
+    )
+                )
+    underline = Alias("u")
+    vertAlign = NoneSet(values=('superscript', 'subscript', 'baseline'))
+    color = ColorDescriptor()
+    scheme = NoneSet(values=("major", "minor"))
+
+    __fields__ = ('name',
+                  'sz',
+                  'b',
+                  'i',
+                  'u',
+                  'strike',
+                  'color',
+                  'vertAlign',
+                  'charset',
+                  'outline',
+                  'shadow',
+                  'condense',
+                  'extend',
+                  'family',
+                  )
+
+    def __init__(self, name='Calibri', sz=11, b=False, i=False, charset=None,
+                 u=None, strike=False, color=BLACK, scheme=None, family=2, size=None,
+                 bold=None, italic=None, strikethrough=None, underline=None,
+                 vertAlign=None, outline=False, shadow=False, condense=False,
+                 extend=False):
+        self.name = name
+        self.family = family
+        if size is not None:
+            sz = size
+        self.sz = sz
+        if bold is not None:
+            b = bold
+        self.b = b
+        if italic is not None:
+            i = italic
+        self.i = i
+        if underline is not None:
+            u = underline
+        self.u = u
+        if strikethrough is not None:
+            strike = strikethrough
+        self.strike = strike
+        self.color = color
+        self.vertAlign = vertAlign
+        self.charset = charset
+        self.outline = outline
+        self.shadow = shadow
+        self.condense = condense
+        self.extend = extend
+        self.scheme = scheme
