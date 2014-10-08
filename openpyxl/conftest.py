6f1ac546dcb9235969cce377bf07f249d5a66869
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-10-08 10:06:16 +0200

    Correct condition for lxml tests.

--- openpyxl/conftest.py
+++ openpyxl/conftest.py
@@ -40,5 +40,6 @@ def pytest_runtest_setup(item):
         elif item.get_marker("not_py33"):
             pytest.skip("Ordering is not a given in Python 3")
         elif item.get_marker("lxml_required"):
-            pytest.skip("LXML is required for some features such as schema validation")
-
+            from openpyxl import LXML
+            if not LXML:
+                pytest.skip("LXML is required for some features such as schema validation")
