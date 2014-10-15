df7f56007c2b28e55375827944c35b85a260074b
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-10-15 12:44:15 +0200

    Move write test to writer module and copy file.

--- openpyxl/reader/tests/test_strings.py
+++ openpyxl/reader/tests/test_strings.py
@@ -30,12 +30,3 @@ def test_formatted_string_table(datadir):
             'Welcome', 'to the best shop in town', "     let's play "]
 
 
-def test_write_string_table(datadir):
-    from openpyxl.writer.strings import write_string_table
-
-    datadir.chdir()
-    table = ['This is cell A1 in Sheet 1', 'This is cell G5']
-    content = write_string_table(table)
-    with open('sharedStrings.xml') as expected:
-        diff = compare_xml(content, expected.read())
-        assert diff is None, diff
