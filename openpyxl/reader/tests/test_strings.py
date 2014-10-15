5829f322a48376689b673ceb41c134a89e9fbecf
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-10-15 12:28:13 +0200

    Move tests and data to reader module.

--- openpyxl/reader/tests/test_strings.py
+++ openpyxl/reader/tests/test_strings.py
@@ -8,22 +8,22 @@ from openpyxl.tests.helper import compare_xml
 
 
 def test_read_string_table(datadir):
-    datadir.join('reader').chdir()
+    datadir.chdir()
     src = 'sharedStrings.xml'
     with open(src) as content:
         assert read_string_table(content.read()) == [
-            'This is cell A1 in Sheet 1', 'This is cell G5']
+                'This is cell A1 in Sheet 1', 'This is cell G5']
 
 
 def test_empty_string(datadir):
-    datadir.join('reader').chdir()
+    datadir.chdir()
     src = 'sharedStrings-emptystring.xml'
     with open(src) as content:
         assert read_string_table(content.read()) == ['Testing empty cell', '']
 
 
 def test_formatted_string_table(datadir):
-    datadir.join('reader').chdir()
+    datadir.chdir()
     src = 'shared-strings-rich.xml'
     with open(src) as content:
         assert read_string_table(content.read()) == [
@@ -33,7 +33,7 @@ def test_formatted_string_table(datadir):
 def test_write_string_table(datadir):
     from openpyxl.writer.strings import write_string_table
 
-    datadir.join("reader").chdir()
+    datadir.chdir()
     table = ['This is cell A1 in Sheet 1', 'This is cell G5']
     content = write_string_table(table)
     with open('sharedStrings.xml') as expected:
