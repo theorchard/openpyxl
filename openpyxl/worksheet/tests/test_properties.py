596255df938f9d0c3f5f9e1ab6c09ea850bc248f
Jean Pierre Huart <openjph@gmail.com>
2014-10-22 13:19:55 +0200

    simplify properties tests

--- openpyxl/worksheet/tests/test_properties.py
+++ openpyxl/worksheet/tests/test_properties.py
@@ -16,24 +16,6 @@ def test_ctor():
     wsprops = WorksheetProperties(tabColor=color_test, outlinePr=outline_pr)
     assert dict(wsprops) == {'tabColor': {'rgb': '00F0F0F0'}, 'outlinePr': {'summaryBelow': '1', 'summaryRight': '1'}}
 
-@pytest.fixture
-def TabColorProps():
-    from .. properties import WorksheetProperties
-    wsp = WorksheetProperties()
-    wsp.filterMode = False
-    wsp.tabColor = '1072BA'
-    return wsp
-
-def test_TabColorProps(TabColorProps):
-    assert dict(TabColorProps) == {'filterMode':'false', 'tabColor': {'rgb': '001072BA'}}
-
-def test_write_properties(TabColorProps):
-    from .. properties import write_sheetPr
-
-    content = write_sheetPr(TabColorProps)
-    expected = """ <s:sheetPr xmlns:s="http://schemas.openxmlformats.org/spreadsheetml/2006/main" filterMode="false"><s:tabColor rgb="001072BA"/></s:sheetPr>"""
-    diff = compare_xml(tostring(content), expected)
-    assert diff is None, diff
 
 @pytest.fixture
 def SimpleTestProps():
@@ -44,6 +26,16 @@ def SimpleTestProps():
     wsp.pageSetUpPr = PageSetupPr(fitToPage=False)
     return wsp
 
+
+def test_write_properties(SimpleTestProps):
+    from .. properties import write_sheetPr
+
+    content = write_sheetPr(SimpleTestProps)
+    expected = """ <s:sheetPr xmlns:s="http://schemas.openxmlformats.org/spreadsheetml/2006/main" filterMode="false"><s:pageSetUpPr fitToPage="0" /><s:tabColor rgb="FF123456"/></s:sheetPr>"""
+    diff = compare_xml(tostring(content), expected)
+    assert diff is None, diff
+
+
 def test_parse_properties(datadir, SimpleTestProps):
     from .. properties import parse_sheetPr
     datadir.chdir()
@@ -52,18 +44,6 @@ def test_parse_properties(datadir, SimpleTestProps):
         content = src.read()
 
     parseditem = parse_sheetPr(fromstring(content))
-    assert dict(parseditem) == dict(SimpleTestProps)
-    assert parseditem.tabColor == SimpleTestProps.tabColor
-    assert dict(parseditem.pageSetUpPr) == dict(SimpleTestProps.pageSetUpPr)
-
-    with open("fullsheet.xml") as src:
-        content = src.read()
-
-    root = fromstring(content)
-    for node in safe_iterator(root):
-        if node.tag == SimpleTestProps.tag:
-            parseditem = parse_sheetPr(node)
-
     assert dict(parseditem) == dict(SimpleTestProps)
     assert parseditem.tabColor == SimpleTestProps.tabColor
     assert dict(parseditem.pageSetUpPr) == dict(SimpleTestProps.pageSetUpPr)
