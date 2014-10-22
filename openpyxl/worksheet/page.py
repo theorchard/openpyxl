9dc8c3dc251f797d1cd8e73649bce396268b47fa
Jean Pierre Huart <openjph@gmail.com>
2014-10-22 17:06:15 +0200

    added @deprecated to fitToPage

--- openpyxl/worksheet/page.py
+++ openpyxl/worksheet/page.py
@@ -25,6 +25,7 @@ from __future__ import absolute_import
 
 from openpyxl.compat import OrderedDict, safe_string
 from openpyxl.descriptors import Strict, Float
+from openpyxl.compat import deprecated
 
 
 class PageSetup(object):
@@ -67,6 +68,10 @@ class PageSetup(object):
                 optionsGroup[options_name] = '1'
 
         return optionsGroup
+    
+    @deprecated('this property has to be called through worksheet.sheet_properties.pageSetUpPr.fitToPage')
+    def fitToPage(self):
+        pass
 
 
 class PageMargins(Strict):
