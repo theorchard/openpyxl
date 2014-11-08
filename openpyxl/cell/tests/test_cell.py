55a11b3dbd0b66e2c0f1b50dc2fff8515f6e35a9
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-11-08 19:42:34 +0100

    Move tests to utils package.

--- openpyxl/cell/tests/test_cell.py
+++ openpyxl/cell/tests/test_cell.py
@@ -9,17 +9,10 @@ import pytest
 
 # package imports
 from openpyxl.collections import IndexedList
-from openpyxl.exceptions import (
-    CellCoordinatesException,
-    )
+
 from openpyxl.date_time import CALENDAR_WINDOWS_1900
-from openpyxl.cell import (
-    column_index_from_string,
-    coordinate_from_string,
-    get_column_letter,
-    Cell,
-    absolute_coordinate
-    )
+from openpyxl.cell import Cell
+
 from openpyxl.comments import Comment
 from openpyxl.styles import numbers, Style
 
@@ -45,27 +38,6 @@ def build_dummy_worksheet():
 
     return Ws()
 
-
-def test_coordinates():
-    column, row = coordinate_from_string('ZF46')
-    assert "ZF" == column
-    assert 46 == row
-
-
-def test_invalid_coordinate():
-    with pytest.raises(CellCoordinatesException):
-        coordinate_from_string('AAA')
-
-def test_zero_row():
-    with pytest.raises(CellCoordinatesException):
-        coordinate_from_string('AQ0')
-
-def test_absolute():
-    assert '$ZF$51' == absolute_coordinate('ZF51')
-
-def test_absolute_multiple():
-
-    assert '$ZF$51:$ZF$53' == absolute_coordinate('ZF51:ZF$53')
 
 @pytest.mark.parametrize("column, idx",
                          [
--- openpyxl/utils/tests/test_utils.py
+++ openpyxl/utils/tests/test_utils.py
@@ -1,2 +1,34 @@
+# Copyright (c) 2010-2014 openpyxl
+
+from .. import (
+    column_index_from_string,
+    coordinate_from_string,
+    get_column_letter,
+    absolute_coordinate
+)
+
+from openpyxl.exceptions import (
+    CellCoordinatesException,
+    )
 
 
+def test_coordinates():
+    column, row = coordinate_from_string('ZF46')
+    assert "ZF" == column
+    assert 46 == row
+
+
+def test_invalid_coordinate():
+    with pytest.raises(CellCoordinatesException):
+        coordinate_from_string('AAA')
+
+def test_zero_row():
+    with pytest.raises(CellCoordinatesException):
+        coordinate_from_string('AQ0')
+
+def test_absolute():
+    assert '$ZF$51' == absolute_coordinate('ZF51')
+
+def test_absolute_multiple():
+
+    assert '$ZF$51:$ZF$53' == absolute_coordinate('ZF51:ZF$53')
