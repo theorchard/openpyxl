8073cc10f5cd43b12816b5bcbf0f9918bd676843
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-11-09 12:58:49 +0100

    Move tests to utils package.

--- openpyxl/cell/tests/test_cell.py
+++ openpyxl/cell/tests/test_cell.py
@@ -39,57 +39,6 @@ def build_dummy_worksheet():
     return Ws()
 
 
-@pytest.mark.parametrize("column, idx",
-                         [
-                         ('j', 10),
-                         ('Jj', 270),
-                         ('JJj', 7030),
-                         ('A', 1),
-                         ('Z', 26),
-                         ('AA', 27),
-                         ('AZ', 52),
-                         ('BA', 53),
-                         ('BZ',  78),
-                         ('ZA',  677),
-                         ('ZZ',  702),
-                         ('AAA',  703),
-                         ('AAZ',  728),
-                         ('ABC',  731),
-                         ('AZA', 1353),
-                         ('ZZA', 18253),
-                         ('ZZZ', 18278),
-                         ]
-                         )
-def test_column_index(column, idx):
-    assert column_index_from_string(column) == idx
-
-
-@pytest.mark.parametrize("column",
-                         ('JJJJ', '', '$', '1',)
-                         )
-def test_bad_column_index(column):
-    with pytest.raises(ValueError):
-        column_index_from_string(column)
-
-
-@pytest.mark.parametrize("value", (0, 18729))
-def test_column_letter_boundries(value):
-    with pytest.raises(ValueError):
-        get_column_letter(value)
-
-@pytest.mark.parametrize("value, expected",
-                         [
-                        (18278, "ZZZ"),
-                        (7030, "JJJ"),
-                        (28, "AB"),
-                        (27, "AA"),
-                        (26, "Z")
-                         ]
-                         )
-def test_column_letter(value, expected):
-    assert get_column_letter(value) == expected
-
-
 @pytest.fixture
 def dummy_cell(request):
     ws = build_dummy_worksheet()
