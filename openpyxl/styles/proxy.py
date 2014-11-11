20d2bb77bace5d645623f7bcbee215a54ca70a8f
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-11-11 20:05:54 +0100

    Stub out some more methods and add a test module.

--- openpyxl/styles/proxy.py
+++ openpyxl/styles/proxy.py
@@ -2,18 +2,19 @@ from __future__ import absolute_import
 # Copyright (c) 2010-2014 openpyxl
 
 
-"""
-Proxy formatting objects so that they cannot be altered
-"""
-
-
 class Proxy(object):
+    """
+    Proxy formatting objects so that they cannot be altered
+    """
+
 
     __slots__ = ('__target')
 
     def __init__(self, target):
         self.__target = target
 
+    def __repr__(self):
+        return repr(self.__target)
 
-    def copy(self):
-        pass
+    def __getattr__(self, attr):
+        return getattr(self.__target, attr)
