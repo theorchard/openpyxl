5b9ae8cde27f29955716dc1252cb6ad02c8e4a30
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-11-11 20:41:22 +0100

    Flesh out behaviour and add some tests.

--- openpyxl/styles/proxy.py
+++ openpyxl/styles/proxy.py
@@ -7,14 +7,22 @@ class Proxy(object):
     Proxy formatting objects so that they cannot be altered
     """
 
-
     __slots__ = ('__target')
 
     def __init__(self, target):
         self.__target = target
 
+
     def __repr__(self):
         return repr(self.__target)
 
+
     def __getattr__(self, attr):
         return getattr(self.__target, attr)
+
+
+    def __setattr__(self, attr, value):
+        if attr != "_Proxy__target":
+            raise AttributeError("Style objects are immutable and cannot be changed."
+                                 "Reassign the style with a copy")
+        super(Proxy, self).__setattr__(attr, value)
--- openpyxl/styles/tests/test_proxy.py
+++ openpyxl/styles/tests/test_proxy.py
@@ -1,2 +1,40 @@
 from __future__ import absolute_import
 # Copyright (c) 2010-2014 openpyxl
+
+import pytest
+
+
+@pytest.fixture
+def dummy_object():
+    class Dummy:
+
+        def __init__(self):
+            self.a = 1
+            self.b = 2
+            self.c = 3
+
+        def __repr__(self):
+            return "dummy object"
+
+    return Dummy()
+
+
+@pytest.fixture
+def proxy(dummy_object):
+    from .. proxy import Proxy
+    return Proxy(dummy_object)
+
+
+def test_ctor(proxy):
+    assert proxy.a == 1
+    assert proxy.b == 2
+    assert proxy.c == 3
+
+
+def test_non_writable(proxy):
+    with pytest.raises(AttributeError):
+        proxy.a = 5
+
+
+def test_repr(proxy):
+    assert repr(proxy) == "dummy object"
