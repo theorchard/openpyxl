ed4900d0c3ed2b166085b32ff02e2abd803fe844
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-11-11 20:06:11 +0100

    Stub out named_styles

---
+++ openpyxl/styles/named_styles.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+# Copyright (c) 2010-2014 openpyxl
+
+
+class NamedStyle(object):
+
+    """
+    Named and editable styles
+    """
+
+    def __init__(self, name):
+        self.name = name
+
