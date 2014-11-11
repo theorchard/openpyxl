6b0c572f4c5b51a7dffb8c9073b3381a3f31e9b6
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-11-11 20:41:42 +0100

    Flesh out imports.

--- openpyxl/styles/named_styles.py
+++ openpyxl/styles/named_styles.py
@@ -2,7 +2,13 @@ from __future__ import absolute_import
 # Copyright (c) 2010-2014 openpyxl
 
 
+from openpyxl.descriptors import Strict
+from openpyxl.xml.constants import SHEET_MAIN_NS
+
+
 class NamedStyle(object):
+
+    tag = '{%s}cellXfs' % SHEET_MAIN_NS
 
     """
     Named and editable styles
@@ -10,4 +16,3 @@ class NamedStyle(object):
 
     def __init__(self, name):
         self.name = name
-
