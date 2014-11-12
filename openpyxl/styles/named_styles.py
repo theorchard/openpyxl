61da6dfd8661d190cb55e54d039a02f304830e46
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-11-12 12:28:19 +0100

    Flesh out class and imports.

--- openpyxl/styles/named_styles.py
+++ openpyxl/styles/named_styles.py
@@ -2,7 +2,16 @@ from __future__ import absolute_import
 # Copyright (c) 2010-2014 openpyxl
 
 
-from openpyxl.descriptors import Strict
+from openpyxl.descriptors import (
+    Strict,
+    Typed,
+)
+from . fonts import Font
+from . borders import Border
+from . alignment import Alignment
+from . numbers import NumberFormatDescriptor
+from . protection import Protection
+
 from openpyxl.xml.constants import SHEET_MAIN_NS
 
 
@@ -14,5 +23,26 @@ class NamedStyle(object):
     Named and editable styles
     """
 
-    def __init__(self, name):
+    font = Typed(expected_type=Font)
+    fill = Typed(expected_type=Fill)
+    border = Typed(expected_type=Border)
+    alignment = Typed(expected_type=Alignment)
+    number_format = NumberFormatDescriptor()
+    protection = Typed(expected_type=Protection)
+
+    def __init__(self,
+                 name,
+                 font=Font(),
+                 fill=PatternFill(),
+                 border=Border(),
+                 alignment=Alignment(),
+                 number_format=None,
+                 protection=Protection()
+                 ):
         self.name = name
+        self.font = font
+        self.fill = fill
+        self.border = border
+        self.alignment = alignment
+        self.number_format = number_format
+        self.protection = protection
