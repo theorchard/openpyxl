2f0d7b4b85923b015915f88e290c7c769aed8557
Eric Gazoni <eric.gazoni@adimian.com>
2014-12-09 16:12:07 +0100

    merge in 2.1.3

---
+++ doc/source/example.py
@@ -0,0 +1,19 @@
+from openpyxl import Workbook
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+import datetime
+ws['A2'] = datetime.datetime.now()
+
+# Save the file
+wb.save("sample.xlsx")
+
