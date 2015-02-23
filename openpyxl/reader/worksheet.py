6b8b12503666f7142c273067c8f5f3b3d4765278
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-02-23 18:55:21 +0100

    Backport performance from 2.3

--- openpyxl/reader/worksheet.py
+++ openpyxl/reader/worksheet.py
@@ -45,6 +45,18 @@ def _get_xml_iter(xml_source):
         return xml_source
 
 
+def styles_cache(styles):
+    """
+    Cell styles are denormalised references to style ids
+
+    """
+    lookup = {}
+    for k, v in enumerate(styles):
+        lookup[k] = v._asdict()
+    return lookup
+
+
+
 class WorkSheetParser(object):
 
     COL_TAG = '{%s}col' % SHEET_MAIN_NS
@@ -64,6 +76,7 @@ class WorkSheetParser(object):
         self.color_index = color_index
         self.guess_types = ws.parent._guess_types
         self.data_only = ws.parent.data_only
+        self.styles = styles_cache(self.ws.parent._cell_styles)
 
     def parse(self):
         dispatcher = {
@@ -123,14 +136,14 @@ class WorkSheetParser(object):
                     self.ws.formula_attributes[coordinate]['ref'] = ref
 
 
-        styles = {}
+        style = {}
         if style_id is not None:
             style_id = int(style_id)
-            style_id = self.ws.parent._cell_styles[style_id]
-            styles = style_id._asdict()
+            style = self.styles[style_id]
+
 
         column, row = coordinate_from_string(coordinate)
-        cell = Cell(self.ws, column, row, **styles)
+        cell = Cell(self.ws, column, row, **style)
         self.ws._add_cell(cell)
 
         if value is not None:
