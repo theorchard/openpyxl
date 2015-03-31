4920697ddebd19bbf978d1ac0154304eeeeb6fc5
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-03-31 09:28:23 +0200

    Windows, not just Jenkins, needs an explicit encoding for non-ascii source. Might want to move to using parse.

--- openpyxl/reader/tests/test_style.py
+++ openpyxl/reader/tests/test_style.py
@@ -280,7 +280,7 @@ def test_rgb_colors(StyleReader, datadir):
 
 def test_custom_number_formats(StyleReader, datadir):
     datadir.chdir()
-    with open("styles_number_formats.xml") as src:
+    with open("styles_number_formats.xml", encoding="utf-8") as src:
         reader = StyleReader(src.read())
 
     reader.parse_custom_num_formats()
